248754fff5f431bcd2c6eb2d99d0c631a038043d
Nathan Castro <nathanslc18@gmail.com>
2021-11-17 08:14:17 -0300

    chore(javascript): part second

--- js/principal.js
+++ js/principal.js
@@ -1,4 +1,36 @@
-var teste = document.getElementById('oi');
-teste.textContent = "ola tesfas"
+var titulo = document.querySelector(".titulo")
+titulo.textContent = "Aparecida Nutri"
 
-console.log(' aqui', teste)
+var paciente = document.querySelector("#primeiro-paciente");
+
+var tdPeso = paciente.querySelector(".info-peso");
+var peso = tdPeso.textContent;
+
+
+
+var tdAltura = paciente.querySelector(".info-altura");
+var altura = tdAltura.textContent;
+
+
+
+var tdImc = paciente.querySelector(".info-imc");
+
+var pesoEhValido = true;
+var alturaEhValida = true;
+
+if(peso <= 0 || peso >= 1000){
+    console.log("Peso inválido!");
+    pesoEhValido = false;
+    tdImc.textContent = "Peso Inválido"
+}
+
+if(altura <= 0 || altura > 3.0){
+    console.log("Altura inválido!");
+    alturaEhValido = false;
+    tdImc.textContent = "Altura Inválida"
+}
+
+if( alturaEhValida && pesoEhValido){
+    var imc = peso / (altura * altura);
+    tdImc.textContent = imc
+}

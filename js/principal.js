88099b3e335f36a6fff0218bb7c2005bead7a080
Nathan Castro <nathanslc18@gmail.com>
2021-11-18 07:21:53 -0300

    chore(javascript): part third

--- js/principal.js
+++ js/principal.js
@@ -1,36 +1,42 @@
 var titulo = document.querySelector(".titulo")
 titulo.textContent = "Aparecida Nutri"
 
-var paciente = document.querySelector("#primeiro-paciente");
+var pacientes = document.querySelectorAll(".paciente");
 
-var tdPeso = paciente.querySelector(".info-peso");
-var peso = tdPeso.textContent;
+for(var i = 0; i < pacientes.length; i++){
 
-
-
-var tdAltura = paciente.querySelector(".info-altura");
-var altura = tdAltura.textContent;
+    var paciente = pacientes[i];
+    var tdPeso = paciente.querySelector(".info-peso");
+    var peso = tdPeso.textContent;
 
 
+    var tdAltura = paciente.querySelector(".info-altura");
+    var altura = tdAltura.textContent;
 
-var tdImc = paciente.querySelector(".info-imc");
 
-var pesoEhValido = true;
-var alturaEhValida = true;
+    var tdImc = paciente.querySelector(".info-imc");
 
-if(peso <= 0 || peso >= 1000){
-    console.log("Peso inválido!");
-    pesoEhValido = false;
-    tdImc.textContent = "Peso Inválido"
-}
+    var pesoEhValido = true;
+    var alturaEhValida = true;
 
-if(altura <= 0 || altura > 3.0){
-    console.log("Altura inválido!");
-    alturaEhValido = false;
-    tdImc.textContent = "Altura Inválida"
-}
+    
 
-if( alturaEhValida && pesoEhValido){
-    var imc = peso / (altura * altura);
-    tdImc.textContent = imc
+    if(peso <= 0 || peso >= 1000){
+        console.log("Peso inválido!");
+        pesoEhValido = false;
+        tdImc.textContent = "Peso Inválido"
+        paciente.classList.add("paciente-invalido")
+    }
+
+    if(altura <= 0 || altura > 3.0){
+        console.log("Altura inválido!");
+        alturaEhValido = false;
+        tdImc.textContent = "Altura Inválida"
+        paciente.classList.add("paciente-invalido")
+    }
+
+    if( alturaEhValida && pesoEhValido){
+        var imc = peso / (altura * altura);
+        tdImc.textContent = imc.toFixed(2);
+    }
 }

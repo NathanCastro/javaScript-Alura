58f3070e82fb2ebb1489bd5afba186e0aface0a7
Nathan Castro <nathanslc18@gmail.com>
2021-11-23 07:06:51 -0300

    chore(javascript): fourth part

--- js/principal.js
+++ js/principal.js
@@ -40,3 +40,39 @@ for(var i = 0; i < pacientes.length; i++){
         tdImc.textContent = imc.toFixed(2);
     }
 }
+
+var botaoAdicionar = document.querySelector("#adicionar-paciente");
+botaoAdicionar.addEventListener("click", function(event) {
+    event.preventDefault();
+
+    var form = document.querySelector("#form-adiciona");
+
+    var nome = form.nome.value;
+    var peso = form.peso.value;
+    var altura = form.altura.value;
+    var gordura = form.gordura.value;
+
+    var parcienteTr = document.createElement('tr');
+
+    var nomeTd = document.createElement('td');
+    var pesoTd = document.createElement('td');
+    var alturaTd = document.createElement('td');
+    var gorduraTd = document.createElement('td');
+    var imcTd = document.createElement('td');
+
+    nomeTd.textContent = nome;
+    pesoTd.textContent = peso;
+    alturaTd.textContent = altura;
+    gorduraTd.textContent = gordura;
+    imcTd.textContent = imc.toFixed(2);
+
+    parcienteTr.appendChild(nomeTd)
+    parcienteTr.appendChild(pesoTd)
+    parcienteTr.appendChild(alturaTd)
+    parcienteTr.appendChild(gorduraTd)
+    parcienteTr.appendChild(imcTd)
+
+    var tabela = document.querySelector('#tabela-pacientes');
+
+    tabela.appendChild(parcienteTr);
+});
